0c6989d06883e3d536490f7029c46957ae8847ed
sujatadhote <123105288+sujatadhote@users.noreply.github.com>
2023-03-01 16:58:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Wheat Kernel Data App")
+st.snow()
+
---
+++ pages/1_Dashboard_wheat.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "wheat.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "wheatkernel_dataset.csv")
+
+st.title("Dashboard - Wheat Data")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+species = st.selectbox("Select the Class:", df['Wheat Type'].unique())
+
+col1, col2 = st.columns(2)
+
+fig_1 = px.histogram(df[df['Wheat Type'] == species], x="Assymetry coefficient")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['Wheat Type'] == species], y="Assymetry coefficient")
+col2.plotly_chart(fig_2, use_container_width=True)
